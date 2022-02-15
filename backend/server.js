4d3fade1c0061a82289cda2f50104cb57c98b200
JonzCandy <joncandy2021@gmail.com>
2022-02-15 17:17:00 -0300

    Arrumando configurações para rodar

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const port = 4444;
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
+/*app.get("/", (req, res) => {
 
 });
 
@@ -15,12 +15,12 @@ app.post("/", (req, res) => {
 });
 
 app.put("/:id", (req, res) => {
- 
+
 });
 
 app.delete("/:id", (req, res) => {
   
-});
+});*/
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
--- frontend/app.js
+++ frontend/app.js
@@ -4,6 +4,6 @@ const port = 8001;
 
 app.use(express.static("public"));
 
-app.listen(port, () => {
-	console.log(`Example app listening at http://localhost:${port}`)
-});
+app.listen(port, () => 
+  console.log(`Example app listening on port ${port}!`)
+);
