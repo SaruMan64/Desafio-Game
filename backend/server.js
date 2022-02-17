06e8f3963f7f20689dd2c7ba380c881fa1b757e3
David <davidg.nascimento@hotmail.com>
2022-02-17 18:45:27 -0300

    players

--- backend/server.js
+++ backend/server.js
@@ -11,9 +11,13 @@ const getOrder = require("./modules/getOrder.js");
 app.get("/order", (req, res) => {
   res.send(getOrder.getOrder());
 });
-/*
-app.post("/", (req, res) => {
-  
+
+app.post("/register", (req, res) => {
+    const player = req.body;
+    player.score = 0;
+    fs.writeFile('./players.json', JSON.stringify(player, null, 2), (err, result) => {
+        console.log(err);
+    });
 });
 
 app.put("/:id", (req, res) => {
