e063aaac9d5c956c380c3eff524e116cb5f2a3f7
yete_wubuntu <yete_wubuntu@yete-windows.localdomain>
2022-02-17 22:27:31 -0300

    Atualizando opening

--- frontend/public/abertura.js
+++ frontend/public/abertura.js
@@ -1,11 +1,12 @@
-$(function () {
-  $("body").prepend(`
-  <section id="doors">
+$(document).ready(function () {
+
+  $(function () {
+    $("body").prepend(`
+    <section id="doors">
       <div id="frontopening">
         <input id="inputName" type="text" placeholder="Insira seu nome..." />
         <button id="btn">INICIAR</button>
       </div>
-
       <div id="backDoor">
         <img
           class="door L"
@@ -21,30 +22,32 @@ $(function () {
         />
       </div>
     </section>
-  `);
+        `);
 
-  $('#btn').click(function () {
-    $(this).prop('disabled', true);
+    $('#btn').click(function () {
+      $(this).prop('disabled', true);
 
-    $("#doors").css("animation", "zoomFadeOut 3s cubic-bezier(.64,0,.4,.39)");
-    $("main").attr("hidden", false);
-    setTimeout(() => {
-      $("#frontopening").html("");
-      const audiott = new Audio("./sonds/Sound-Button-Effect-Sliding.wav");
-      audiott.play();
+      $("#doors").css("animation", "zoomFadeOut 3s cubic-bezier(.64,0,.4,.39)");
+      $("main").attr("hidden", false);
+      setTimeout(() => {
+        $("#frontopening").html("");
+        const audiott = new Audio("./sonds/Sound-Button-Effect-Sliding.wav");
+        audiott.play();
 
-      const animationDoor = " 2800ms cubic-bezier(1,0,.5,1)"
-      const animationBrownser = ["-webkit-animation", "-moz-animation", "-o-animation", "animation"]
-      animationBrownser.forEach(el => {
-        $(".L").css(el, "doorL" + animationDoor);
-        $(".R").css(el, "doorR" + animationDoor);
-      });
-    }, 500)
+        const animationDoor = " 2800ms cubic-bezier(1,0,.5,1)"
+        const animationBrownser = ["-webkit-animation", "-moz-animation", "-o-animation", "animation"]
+        animationBrownser.forEach(el => {
+          $(".L").css(el, "doorL" + animationDoor);
+          $(".R").css(el, "doorR" + animationDoor);
+        });
+      }, 500)
 
-    setTimeout(() => {
-      $(".L").css("left", "-50%")
-      $(".R").css("left", "100%")
-      $("#doors").remove();
-    }, 2800);
-  })
-});
+      setTimeout(() => {
+        $(".L").css("left", "-50%")
+        $(".R").css("left", "100%")
+        $("#doors").remove();
+      }, 2800);
+    })
+  });
+
+})
