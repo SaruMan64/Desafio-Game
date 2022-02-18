8c28fc4aa41c6caf6c21788762100f71498c84be
yete_wubuntu <yete_wubuntu@yete-windows.localdomain>
2022-02-17 22:03:49 -0300

    Atualizando opening

--- frontend/public/abertura.js
+++ frontend/public/abertura.js
@@ -1,9 +1,33 @@
 $(function () {
+  $("body").prepend(`
+  <section id="doors">
+      <div id="frontopening">
+        <input id="inputName" type="text" placeholder="Insira seu nome..." />
+        <button id="btn">INICIAR</button>
+      </div>
+
+      <div id="backDoor">
+        <img
+          class="door L"
+          src="./images/opening/Door.svg"
+          alt=""
+          srcset="./images/opening/Door.svg"
+        />
+        <img
+          class="door R"
+          src="./images/opening/Door.svg"
+          alt=""
+          srcset="./images/opening/Door.svg"
+        />
+      </div>
+    </section>
+  `);
+
   $('#btn').click(function () {
     $(this).prop('disabled', true);
 
     $("#doors").css("animation", "zoomFadeOut 3s cubic-bezier(.64,0,.4,.39)");
-
+    $("main").attr("hidden", false);
     setTimeout(() => {
       $("#frontopening").html("");
       const audiott = new Audio("./sonds/Sound-Button-Effect-Sliding.wav");
