3664cb262d9e5e938438bbf64e2369d1c158e402
Luan Raabe <luan.raabe202@gmail.com>
2022-02-19 11:45:20 -0300

    Progredindo noo jogo

--- backend/modules/getOrder.js
+++ backend/modules/getOrder.js
@@ -1,34 +1,53 @@
 const allIngredients = require("../ingredientsDataBase.json");
 
 function aleatory(base, quantity) {
-  let ingredients = [];
-  for(let i = 0; i < quantity; i++) {
-    ingredients.push(base[Math.floor(Math.random() * base.length)]);
-  }
-  let resp = {};
-  let holder = [...ingredients];
-  holder.forEach((el) => {
-    if (resp.hasOwnProperty(el)) {
-      resp[el] += 1;
-    } else {
-      resp[el] = 1;
+    let ingredients = [];
+    for (let i = 0; i < quantity; i++) {
+        ingredients.push(base[Math.floor(Math.random() * base.length)]);
     }
-  })
-  return resp;
+    let resp = {};
+    let holder = [...ingredients];
+    holder.forEach((el) => {
+        if (resp.hasOwnProperty(el)) {
+            resp[el] += 1;
+        } else {
+            resp[el] = 1;
+        }
+    })
+    return resp;
+}
+
+function selectIngredients(base, quantity) {
+    let ingredients = [];
+    let holder = base[Math.floor(Math.random() * base.length)];
+    ingredients.push(holder);
+    for (let i = 0; i < quantity; i++) {
+        let has = true;
+        while(has){
+            if(ingredients.find(item => item == holder)){
+                holder = base[Math.floor(Math.random() * base.length)];
+            } else {
+                has = false;
+            }
+        }
+        ingredients.push(holder);
+    }
+    return ingredients;
 }
 
 function getOrder(value) {
-  const quantity = (Math.floor(Math.random() * 6) + 4);
-  const order = {
-    "id": value,
-    "broth": allIngredients.broth[Math.floor(Math.random() * allIngredients.broth.length)],
-    "cookingTime": ((Math.floor(Math.random() * 10) * 5) + 10),
-    "quantIngredients": quantity,
-    "ingredients": aleatory(allIngredients.ingredients, quantity)
-  }
-  return order;
+    const quantity = (Math.floor(Math.random() * 6) + 4);
+    const used = selectIngredients(allIngredients.ingredients, 4);
+    const order = {
+        "id": value,
+        "broth": allIngredients.broth[Math.floor(Math.random() * allIngredients.broth.length)],
+        "cookingTime": ((Math.floor(Math.random() * 10) * 5) + 10),
+        "quantIngredients": quantity,
+        "ingredients": aleatory(used, quantity)
+    }
+    return order;
 }
 
 module.exports = {
-  getOrder
+    getOrder
 };
