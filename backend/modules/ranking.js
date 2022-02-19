a795f3d3c2882d47c3b2ac3e537b571473384f51
SamuelHVRodrigues <samuelhvrodrigues@gmail.com>
2022-02-19 01:35:08 -0300

    register, ranking, score

---
+++ backend/modules/ranking.js
@@ -0,0 +1,19 @@
+const allPlayers = require("../players.json");
+const players = allPlayers.players;
+
+const ranking = function() {    // Sort by final score
+    let counter = players.length;
+    for(let i = 0; i < players.length-1; i++) {
+        for(let j = 0; j < counter-1; j++) {
+            if(players[j].score.final < players[j+1].score.final) {
+                const aux = players[j];
+                players[j] = players[j+1];
+                players[j+1] = aux;
+            }
+        }
+        counter--;
+    }
+    return players;
+}
+
+module.exports = ranking;
--- frontend/app.js
+++ frontend/app.js
@@ -8,6 +8,8 @@ app.use('/opening', express.static('public/opening.html'));
 
 app.use('/game', express.static('public/jogo.html'));
 
+app.use('/testes', express.static('public/index.html'));
+
 app.listen(port, () => 
   console.log(`Example app listening on port ${port}!`)
 );
