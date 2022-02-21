c2f3241d127d767681df585e5ce205275eb990b2
SaruMan64 <yete.abuna@gmail.com>
2022-02-21 10:37:56 -0300

    Atualizando validação de registro e opening

--- backend/modules/register.js
+++ backend/modules/register.js
@@ -13,12 +13,12 @@ const player = {
 
 const register = function(name) {
     const filterName = /[^a-zA-Zà-ýÀ-Ý0-9]/;
-    if(filterName.test(name)) { // Checks if the name contains only letters
+    if(filterName.test(name)) { // Checks if the name contains only letters and numbers
         return ("Os nomes de usuário não podem conter acentos, números ou caracteres especiais.");
     }
 
-    if(name.length > 15) { // Check if the name is too long
-        return ("Nome de usuário muito longo.");
+    if(name.length < 1 && name.length > 15) { // Check if the name is too long
+        return ("Nome de usuário não tem tamanho suficiente.");
     }
 
     for(let i = 0; i < players.length; i++) { // Checks if the name already exists
