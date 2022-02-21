948ffd3a60649cd751a037761373e0f4f7ed418b
David <davidg.nascimento@hotmail.com>
2022-02-20 23:12:12 -0300

    writeFile

--- backend/modules/register.js
+++ backend/modules/register.js
@@ -1,4 +1,5 @@
 const allPlayers = require("../players.json");
+const fs = require('fs');
 const players = allPlayers.players;
 
 const player = {
@@ -7,12 +8,11 @@ const player = {
     score: {
         current: 0,
         final: 0
-    },
-    cep: 0
+    }
 };
 
 const register = function(name) {
-    const filterName = /[^a-zA-Z]/;
+    const filterName = /^[a-zA-Z0-9]*$/;
     if(!filterName.test(name)) { // Checks if the name contains only letters
         return ("Os nomes de usuário não podem conter acentos, números ou caracteres especiais.");
     }
@@ -22,17 +22,22 @@ const register = function(name) {
     }
 
     for(let i = 0; i < players.length; i++) { // Checks if the name already exists
+        console.log(players[i].name);
         if(name.toLowerCase() === players[i].name.toLowerCase()) {
             return("Esse nome de usuário não está disponível. Tente outro nome.");
         };
     }
 
     const currentPlayer = Object.create(player); // Fill player
-    currentPlayer.id = players.length+1;
+    currentPlayer.id = players.length + 1;
     currentPlayer.name = name;
     currentPlayer.score = { current: 0, final: 0 };
 
     players.push(currentPlayer);
+
+    fs.writeFile('./players.json', JSON.stringify(players, null, 2), (err, result) => {
+        console.log(err);
+    });
 
     return true;
 }
