92341aa4b00c7774d11bb999d2363b9cbd8d8902
SaruMan64 <yete.abuna@gmail.com>
2022-02-21 21:45:58 -0300

    Adicionando estilização da estrutura de pedidos

--- frontend/public/s-script.js
+++ frontend/public/s-script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $("#abacate").load("./images/Pedido/pedido.svg");
     setTimeout(() => {
-        //$("#clothespin").css("display", "inherit")
+        $("#clothespin").css("display", "inherit")
         $("#cook").attr("href", "./images/chashu.png");
     }, 400)
 })
