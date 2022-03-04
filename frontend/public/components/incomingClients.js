6954e195f0d47e7254c12aed2a75fe6df0001f63
Luan Raabe <luan.raabe202@gmail.com>
2022-03-04 00:13:48 -0300

    Novos bonequin

--- frontend/public/components/incomingClients.js
+++ frontend/public/components/incomingClients.js
@@ -1,33 +1,38 @@
-import {getOrder} from "./requests.js";
+import { getOrder } from "./requests.js";
 
-function clientOrder(){
+function clientOrder() {
     let whatClientIsComming = Math.floor(Math.random() * 6) + 1;
-    $("#all-clients > div").each(function(i,item){
-        let act = item.children[0].src.split("/");
-        if(act[act.length -1] == "seat.png"){
+    $("#all-clients > div").each(function (i, item) {
+        let act = item.children;
+        let hold = act[act.length - 1].src.split("/");
+        if (hold[hold.length - 1] == "seat.png") {
             item.innerHTML = "";
-            let div = $(`<div class='take-my-order'>
+            let div = $(
+                `<img src='../images/Pedido/client-${whatClientIsComming}-seat.png' />`
+            );
+            item.append(div[0]);
+            setTimeout(() => {
+                div = $(`<div class='take-my-order'>
                             <img src='../images/Pedido/pedido.png' />
                             <div class="accept-decline">
                                 <div class="accept"></div>
                                 <div class="decline"></div>
                             </div
                         </div>`);
-            item.append(div[0]);
-            div = $(`<img class="client-on-seat" src='../images/Pedido/client-${whatClientIsComming}-seat.png' />`);
-            item.append(div[0]);
-            $(".accept").click(function() {
+            item.prepend(div[0]);
+            $(".accept").click(function () {
                 console.log("OI!");
                 getOrder();
             });
-            $(".decline").click(function() {
-                item.innerHTML = "";
-                div = $(`<img class="seat" src="../images/Pedido/seat.png" />`);
-                item.append(div[0]);
+            $(".decline").click(function () {
+                /* item.innerHTML = "";
+                div = $(`<img src="../images/Pedido/seat.png" />`);
+                item.append(div[0]); */
             });
+            }, 500);
             return false;
         }
     });
 }
 
-export {clientOrder};
+export { clientOrder };
